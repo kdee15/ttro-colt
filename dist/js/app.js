6dada1504321923722974d501ff5e46a6f6929c7
kirk <kdee15@gmail.com>
2018-10-18 06:58:26 +0200

    content 1-3 2-1

--- dist/js/app.js
+++ dist/js/app.js
@@ -314,6 +314,45 @@ var mySwiper = new Swiper ('.features-carousel', {
 })
 // A.1. END -----------------------------------------------------------------------------------------------------------
 
+// A.1. WITH NO LOOP DESKTOP ------------------------------------------------------------------------------------------
+
+var mySwiper = new Swiper ('.no-loop', {
+
+  direction: 'horizontal',
+  loop: false,
+  slidesPerView: 3,
+  spaceBetween: 15,
+  noSwiping: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+      noSwiping: true,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 5,
+      loop: true,
+      noSwiping: false,
+    },
+    575: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      loop: true,
+      noSwiping: false,
+    }
+  }
+})
+// A.1. END -----------------------------------------------------------------------------------------------------------
+
 // A.1. MOBILE CLASS --------------------------------------------------------------------------------------------------
 
 var mySwiper = new Swiper ('.images-carousel', {
